114042c0fa69bbdde4b553bd61a1251fc954fb06
Shahid Raza <razashahid532@gmail.com>
2023-06-10 22:39:05 +0530

    Added Controllers and seperated routes and controllers

---
+++ src/controllers/user-controller.js
@@ -0,0 +1,61 @@
+
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { User } = require("../models");
+
+const register = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user in the database
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    res.json({ message: "Registration successful", user });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Registration failed", error: error.message });
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Find the user in the database
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    // Compare the password with the hashed password
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    // Generate JWT token
+    const token = jwt.sign({ userId: user.id }, "your_secret_key");
+
+    res.json({ message: "Login successful", token });
+  } catch (error) {
+    res.status(500).json({
+      message: "Login failed",
+      error: error.message,
+    });
+  }
+};
+
+module.exports = {
+  register,
+  login,
+};
